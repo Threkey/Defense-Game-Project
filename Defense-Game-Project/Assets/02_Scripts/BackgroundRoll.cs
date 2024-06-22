3d66a1af4e098f4113458569b940dd21fe78c1da
Threkey <rlaghgus444@naver.com>
2024-06-22 21:11:31 +0900

    Monsters prefab

---
+++ Defense-Game-Project/Assets/02_Scripts/BackgroundRoll.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRoll : MonoBehaviour
+{
+    public GameObject background;    // 배경 프리팹
+
+    public float speed;              // 배경 이동 속도
+
+    private float sizeX, sizeY, screenSizeX, screenSizeY;    // 배경 사이즈 X, 배경 사이즈 Y, 화면 사이즈 X, 화면 사이즈 Y
+    private Vector3 spawnPos;                                // 배경 생성 위치
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
