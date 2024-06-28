9435488349caf964525df6a598d1f5f1f9f7032e
Threkey <rlaghgus444@naver.com>
2024-06-28 16:47:23 +0900

    background roll

--- Defense-Game-Project/Assets/02_Scripts/BackgroundRoll.cs
+++ Defense-Game-Project/Assets/02_Scripts/BackgroundRoll.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class BackgroundRoll : MonoBehaviour
 {
-    public GameObject background;    // 배경 프리팹
+    [SerializeField]
+    float speed = 1.0f;
 
-    public float speed;              // 배경 이동 속도
-
-    private float sizeX, sizeY, screenSizeX, screenSizeY;    // 배경 사이즈 X, 배경 사이즈 Y, 화면 사이즈 X, 화면 사이즈 Y
-    private Vector3 spawnPos;                                // 배경 생성 위치
+    float width;
     // Start is called before the first frame update
     void Start()
     {
-        
+        width = GetComponent<Collider2D>().bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        Debug.Log(width);
+
+        if(transform.position.x <= -width)
+        {
+            Debug.Log("if");
+            transform.position = Vector3.right * width;
+        }
     }
 }
